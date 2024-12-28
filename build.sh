633ee73e279431b3bf91cec82a82bee2037b9b8a
The-Young-Programer <abduramonibrahim@gmail.com>
2024-12-28 16:17:54 +0100

    Remove

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
-#!/usr/bin/env bash
-# exit on error
+#!/bin/bash
+# Exit on error
 set -o errexit
 
-# Install python dependencies
+# Install system dependencies
+apt-get update
+apt-get install -y \
+    python3-dev \
+    postgresql-client \
+    postgresql-contrib \
+    libpq-dev \
+    gcc \
+    python3-pip
+
+# Create and activate virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
+pip install wheel
+pip install psycopg2-binary
 pip install -r requirements.txt
 
 # Create necessary directories
