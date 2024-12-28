661e5d4faab37699fdc5a3a5b4c030bc7cb87997
The-Young-Programer <abduramonibrahim@gmail.com>
2024-12-28 17:06:51 +0100

    Remove

--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 # exit on error
 set -o errexit
 
-# Install Python dependencies
-pip install --no-cache-dir -r requirements.txt
+pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate 
--- mailer/settings.py
+++ mailer/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['out-bound-app.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'out-bound-app.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Amazon SES configuration
 AMAZON_SES_ACCOUNT = {
