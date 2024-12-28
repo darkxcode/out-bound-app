e7bdc15841f3d3e69ac9a9c0ef0dd8017455b7ab
The-Young-Programer <abduramonibrahim@gmail.com>
2024-12-28 12:29:57 +0100

    Initial commit

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate 
+python manage.py migrate
+
+# Create superuser if not exists
+python manage.py createsuperuser --noinput || true 
